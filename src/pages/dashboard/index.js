2eb782f63fbe47ae9821c82c46c628a2ce7b834d
SachinK-angularminds <sachin@angularminds.in>
2024-04-10 10:38:09 +0530

    first commit

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../app/globals.css'
+
+const Dashboard = () => {
+  return (
+    <div className='flex text-center'>
+      Dashboard
+    </div>
+  )
+}
+
+export default Dashboard
